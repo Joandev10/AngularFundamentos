46320cd3a9ddbff6d4b39327e17af3bea0617359
Jose Chavez <joan.dev93@gmail.com>
2020-10-31 19:04:57 -0500

    Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { EncabezadoComponent } from "./encabezado/encabezado.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ContactoComponent } from "./contacto/contacto.component";
+import { NosotrosComponent } from "./nosotros/nosotros.component";
+import { InicioComponent } from "./inicio/inicio.component";
+
+const routes: Routes = [
+  { path: "contacto", component: ContactoComponent },
+  { path: "nosotros", component: NosotrosComponent },
+  { path: "", component: InicioComponent, pathMatch: "full" },
+  { path: "**", redirectTo: "/", pathMatch: "full" },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    FooterComponent,
+    ContactoComponent,
+    NosotrosComponent,
+    InicioComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
